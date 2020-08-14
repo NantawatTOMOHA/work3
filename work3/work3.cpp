3cd82dc49debfef890e39064a533d8b0abd9ba40
NANTAWAT RATTANARUANGVIMAN <63010519@KMITL.AC.TH>
2020-08-14 20:00:24 +0700

    Add project files.

---
+++ work3/work3.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+double area[4] = {0,0,0,0};
+void data();
+void Area();
+void Perimeter();
+int main() {
+	data();
+	Area();
+	return 0;
+}
+void data() {
+	printf("Claculate area rectriangle and Square perimeter\n");
+	printf("width : ");
+	scanf_s("%lf", area[0]);
+	printf("heigth : ");
+	scanf_s("%lf", area[1]);
+}
+void Area() {
+	area[2] = area[1] * area[0];
+	printf("Area rectriangle : %.2lf\n", area[2]);
+}
+void Perimeter() {
+	area[3] = (2*area[1])+(2*area[0]);
+	printf("Square perimeter : %.2lf\n", area[3]);
+}
